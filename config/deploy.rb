a9674ee1b902a5ee01b306c710e4e59963937956
Brett Buddin <brett@intraspirit.net>
2010-04-01 14:37:54 -0400

    Setting the Git repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,23 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "vote.trianglebattle.com"
+set :location, "174.143.146.148"
+set :user, "deploy"
+set :port, 22
+set :deploy_to, "/var/www/vote.trianglebattle.com"
+set :deploy_via, :copy
+set :copy_remote_dir, "/var/www/tmp/capistrano"
+set :copy_exclude, [".git", "Capfile", "config/deploy.rb"]
+set :use_sudo, false
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
+set :repository,  "deploy@10.80.160.67:code/mckinney-battle-of-the-bands-voting.git"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+namespace :remote do
+  desc "Create directory required by copy_remote_dir"
+  task :create_copy_dir, :roles => :app do
+    run "mkdir -p #{copy_remote_dir}"
+  end
+end
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+role :web, location
+role :app, location
+role :db,  location, :primary => true
