283a2e7455f7ed22f50f75bdb6ae05290198c36c
Brett Buddin <brett@intraspirit.net>
2010-04-01 17:32:47 -0400

    Added the sms to the resource declaration in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :votes
+  map.resources :votes, :collection => {:sms => :post}
 
   # The priority is based upon order of creation: first created -> highest priority.
 
