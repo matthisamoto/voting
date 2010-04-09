429bfab28c63c2cdb4df79471e03d7f856d34db9
Brett Buddin <brett@intraspirit.net>
2010-04-09 15:40:35 -0400

    Fixing divide by zero error in JS.

--- public/assets/js/display.js
+++ public/assets/js/display.js
@@ -10,11 +10,13 @@ $(function() {
         $.each(data["candidates"], function(key, value) {
           if (max <= value) max = value;
         });
-        $.each(data["candidates"], function(key, value) {
-          $("#candidate-" + key + " .percentage").html(Math.round((value/total*100)*100)/100 + "%");
-          $("#candidate-" + key + " .percentage").css({ bottom: (value/max*400) + "px" });
-          $("#candidate-" + key + " .bar").css({ height: (value/max)*400 + "px" });
-        });
+        if (total != 0) {
+          $.each(data["candidates"], function(key, value) {
+            $("#candidate-" + key + " .percentage").html(Math.round((value/total*100)*100)/100 + "%");
+            $("#candidate-" + key + " .percentage").css({ bottom: (value/max*400) + "px" });
+            $("#candidate-" + key + " .bar").css({ height: (value/max)*400 + "px" });
+          });
+        }
       }
     });
   }, 3000);
