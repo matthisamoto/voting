12200a043bf9c448b027c327374b1739d658096b
Brett Buddin <brett@intraspirit.net>
2010-04-12 15:06:40 -0400

    Removed root path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :votes, :collection => {:counts => :get}
   map.resources :sms_votes
-  map.root :controller => :votes, :action => :index
 end
